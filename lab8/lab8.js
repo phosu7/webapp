a2b294e1aaaeaaa79278f45fd9fef48919451fe9
phosu7 <99696023+phosu7@users.noreply.github.com>
2023-10-19 00:13:49 -0500

    Update lab8.js

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -4,7 +4,6 @@ function getDataFromForm() {
   //Create this function.
   var fname = document.getElementsByName("fname")[0].value;
   var lname = document.getElementsByName("lname")[0].value;
-  alert(lname);
   runAjax(fname, lname);
 }
 
@@ -12,6 +11,8 @@ function runAjax(fname, lname) {
   const xhttp = new XMLHttpRequest();
   xhttp.onload = function() {
     //Edit this
+    fname.innerHTML = this.responseText;
+    lname.innerHTML = this.responseText;
     document.getElementById("stringResponse").innerHTML = this.responseText;
     }
   xhttp.open("GET", "ajax.php", true);
