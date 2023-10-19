9fc2bc7558a74547547f25eee094c36cee02df91
phosu7 <99696023+phosu7@users.noreply.github.com>
2023-10-19 05:33:56 -0500

    Update lab8.js

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -13,7 +13,8 @@ function runAjax(fname, lname) {
     //Edit this
     document.getElementById("responseString").innerHTML = this.responseText;
   }
-  xhttp.open("GET", "https://myserverallaboutme.azurewebsites.net/lab8/ajax.php?fname="+fname+"&last="+lname,true);
+  xhttp.open("GET", "https://myserverallaboutme.azurewebsites.net/lab8/ajax.php?fname="+fname,true);
+  xhttp.open("GET", "https://myserverallaboutme.azurewebsites.net/lab8/ajax.php?lname="+lname,true);
   xhttp.send();
 }
 
