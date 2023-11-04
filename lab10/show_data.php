132832c8596f926d96d542bacd68e6d1d2273759
phosu7 <99696023+phosu7@users.noreply.github.com>
2023-11-04 17:16:06 -0500

    q

--- lab10/show_data.php
+++ lab10/show_data.php
@@ -1,6 +1,6 @@
 <?php
        
-        $file_path = './data.txt';
+        $file_path = '/home/site/data.txt';
         $data_lines = file($file_path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
         foreach ($data_lines as $line) {
             
