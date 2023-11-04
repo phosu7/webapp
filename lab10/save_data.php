9c7927b02f3f4acb009ba8ae028d99ad2aaa35c2
phosu7 <99696023+phosu7@users.noreply.github.com>
2023-11-04 08:12:49 -0500

    added files

---
+++ lab10/save_data.php
@@ -0,0 +1,16 @@
+<?php
+    $first_name = $_POST["first_name"];
+    $last_name = $_POST["last_name"];
+    
+    $data = $first_name . " " . $last_name . PHP_EOL; 
+    $file_path = '/var/www/html/save_file/data.txt'; 
+    $file = fopen($file_path, "a");
+    if ($file) {
+        
+        fwrite($file, $data);
+        fclose($file);
+        echo "added!"
+    } else {
+        echo "Error: file not opened for writing.";
+    }
+?>
---
+++ lab10/show_data.php
@@ -0,0 +1,16 @@
+<?php
+       
+        $file_path = '/var/www/html/save_file/data.txt';
+        $data_lines = file($file_path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        foreach ($data_lines as $line) {
+            
+            list($first_name, $last_name) = explode(' ', $line);
+
+            
+            echo "<tr>";
+            echo "<td>$first_name</td>";
+            echo "<td>$last_name</td>";
+            echo "</tr>";
+        }
+
+?>
