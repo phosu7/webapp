bccd387b9e598a2155d4de313681e2acdda2024f
phosu7 <99696023+phosu7@users.noreply.github.com>
2023-11-04 08:26:49 -0500

    change path

--- lab10/show_data.php
+++ lab10/show_data.php
@@ -1,6 +1,6 @@
 <?php
        
-        $file_path = '/var/www/html/save_file/data.txt';
+        $file_path = '/home/save_file/data.txt';
         $data_lines = file($file_path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
         foreach ($data_lines as $line) {
             
