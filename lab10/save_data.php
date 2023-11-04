6ed8be21428412c24d9844bcdda138a2a09894e6
phosu7 <99696023+phosu7@users.noreply.github.com>
2023-11-04 08:06:43 -0500

    Update save_data.php

--- lab10/save_data.php
+++ lab10/save_data.php
@@ -2,20 +2,16 @@
     $first_name = $_POST["first_name"];
     $last_name = $_POST["last_name"];
     
-    // Combine the data
-    $data = $first_name . " " . $last_name . PHP_EOL; // Add a newline for each entry
+    $data = $first_name . " " . $last_name . PHP_EOL; 
     
-    // Define the file path outside of the web server's root directory
-    $file_path = '/var/www/html/save_file/data.txt'; // Replace with the actual path
     
-    // Open the file for appending
+    $file_path = '/var/www/html/save_file/data.txt'; 
+    
     $file = fopen($file_path, "a");
     
     if ($file) {
-        // Write the data to the file
-        fwrite($file, $data);
         
-        // Close the file
+        fwrite($file, $data);
         fclose($file);
         
         echo "Data saved successfully!";
