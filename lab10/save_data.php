92f11a15c8c48a822898723940f93e26b9210991
phosu7 <99696023+phosu7@users.noreply.github.com>
2023-11-04 19:33:59 -0500

    Update save_data.php

--- lab10/save_data.php
+++ lab10/save_data.php
@@ -2,7 +2,7 @@
     $first_name = $_POST["first_name"];
     $last_name = $_POST["last_name"];
     
-    $data = $first_name . " " . $last_name . PHP_EOL; 
+    $data = $first_name . " " . $last_name . "\n"; 
     $file_path = '/home/site/data.txt';
     file_put_contents($file_path, $data, FILE_APPEND);
 ?>
