d6726126923e5aebc69628149550e494403a03c2
phosu7 <99696023+phosu7@users.noreply.github.com>
2023-11-10 14:19:50 -0600

    Update index.php

--- lab12/index.php
+++ lab12/index.php
@@ -1,4 +1,21 @@
 <?php
+$cookie_name = "cs4413";
+$cookie_value = "Cookie set";
 
-
+setcookie($cookie_name, $cookie_value, time() + (86400), "/"); //1 day
 ?>
+
+<html>
+    <body>
+
+    <?php
+        if(!isset($_COOKIE[$cookie_name])) {
+            echo "Cookie named '" . $cookie_name . "' is not set!";
+        } else {
+            echo "Cookie '" . $cookie_name . "' is set!<br>";
+            echo "Value is: " . $_COOKIE[$cookie_name];
+        }
+    ?>
+
+    </body>
+</html>
